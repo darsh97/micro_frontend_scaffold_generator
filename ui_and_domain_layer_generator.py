b396fe5bd6fc1ea6a6ea98f5d77248534ba240e3
Sazzad Hossain <hossain.sazzad@bkash.com>
2023-03-01 16:43:50 +0600

    Update mf_name to mf_snake_case

--- ui_and_domain_layer_generator.py
+++ ui_and_domain_layer_generator.py
@@ -16,10 +16,10 @@ mf_pascal_case = snake_case_to_pascal_case(mf_snake_case)
 ui_and_domain_template_path = 'template/ui_and_domain/'
 
 for root, dirs, files in os.walk(ui_and_domain_template_path):
-    os.makedirs(where_to_generate_dir + (root.replace('${mf_name}', mf_snake_case)).replace(ui_and_domain_template_path, ''))
+    os.makedirs(where_to_generate_dir + (root.replace('${mf_snake_case}', mf_snake_case)).replace(ui_and_domain_template_path, ''))
     for _file in files:
         file_path = os.path.join(root, _file)
-        new_file_path = where_to_generate_dir + (file_path.replace('${mf_name}', mf_snake_case)).replace(ui_and_domain_template_path, '')
+        new_file_path = where_to_generate_dir + (file_path.replace('${mf_snake_case}', mf_snake_case)).replace(ui_and_domain_template_path, '')
         f = open(new_file_path, 'w')
         with open(file_path, 'r') as auto:
             content = auto.read()
